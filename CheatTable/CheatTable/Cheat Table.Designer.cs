72b6e4a0adb41e568e844e9dc32d08f838451b48
Nagataka <irsyadpso2@gmail.com>
2022-07-11 02:28:23 +0700

    Add files via upload

---
+++ CheatTable/CheatTable/Cheat Table.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace CheatTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.processName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto Attach To :";
+            // 
+            // processName
+            // 
+            this.processName.Location = new System.Drawing.Point(94, 3);
+            this.processName.Name = "processName";
+            this.processName.Size = new System.Drawing.Size(318, 20);
+            this.processName.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Address,
+            this.Type,
+            this.Value});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(406, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Address
+            // 
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            this.Address.Width = 160;
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 188);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.processName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cheat Table";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox processName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
+
---
+++ CheatTable/CheatTable/Cheat Table.cs
@@ -0,0 +1,76 @@
+﻿using Memory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheatTable
+{
+    public partial class Form1 : Form
+    {
+
+        Mem m = new Mem();
+        int PID = 0;
+
+        List<string> dataCheat = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Task.Run(() => {
+                if (PID == 0)
+                {
+                    Process p;
+                    while (true)
+                    {
+                        if (Process.GetProcessesByName(processName.Text.ToLower().Replace(".exe", "")).Length > 0)
+                        {
+                            p = Process.GetProcessesByName(processName.Text.ToLower().Replace(".exe", ""))[0];
+                            PID = p.Id;
+                            m.OpenProcess(PID);
+                            processName.Text += " Attached To " + PID;
+                            break;
+                        }
+                        Thread.Sleep(100);
+                    }
+                }
+            });
+
+            dataCheat = File.ReadAllLines("cheat.ini").ToList();
+
+            foreach (var item in dataCheat)
+            {
+                var tmp = item.Split('#');
+                dataGridView1.Rows.Add(tmp);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (PID != 0)
+            {
+                try
+                {
+                    foreach (DataGridViewRow item in dataGridView1.Rows)
+                    {
+                        Console.WriteLine(item.Cells["Address"].Value.ToString());
+                        if (item.Cells["Address"].Value != null && item.Cells["Type"].Value != null && item.Cells["Value"].Value != null)
+                        {
+                            m.WriteMemory(item.Cells["Address"].Value.ToString(), item.Cells["Type"].Value.ToString(), item.Cells["Value"].Value.ToString());
+                        }
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
